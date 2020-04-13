7c0e52a1d9bab6a8eabeb546274b2a40926347c6
Sergej Dmitrenko <61592085+chichikow@users.noreply.github.com>
2020-04-14 00:25:55 +0300

    Update min_depth.py

--- min_depth.py
+++ min_depth.py
@@ -10,11 +10,11 @@ class Solution(object):
         if root.left==None and root.right==None: 
             return 1
         
-        if root.left==None:
-            return self.minDepth(root.right)+1
-        
-        if root.right==None:
-            return self.minDepth(root.left)+1
+        if root.left is None or root.right is None:
+            if root.left is None:
+                return self.minDepth(root.left) + 1
+            else:
+                return self.minDepth(root.right) + 1
         
         return min(self.minDepth(root.left)+1,self.minDepth(root.right)+1)
 
